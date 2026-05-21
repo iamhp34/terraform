c90ca16d30d2a444ea5fb2bf77a38b06155de842
HP <HP@gmail.com>
2026-05-21 15:30:56 +0530

    j

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,4 @@
-module "my_ec2_instance" {
-  source = "./modules/ec2"
-}
+resource "aws_instance" "my_web_server" {
+  ami           = "ami-0c7217cdde317cfec" # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro" 
+  }
