d99d4ca8a5a7541d241431856817fa3f3e408707
HP <HP@gmail.com>
2026-05-21 09:29:42 +0530

    Initial commit of local files

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_version = ">=1.0.0"
+    required_providers{
+        aws = {
+            source = "harshicorp/aws"
+            version = "~> 5.0" 
+        }
+    }
+}
+
+provider "aws" {
+    region = "ap-south-1"
+}
