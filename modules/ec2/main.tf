4776984f171c836f583e535a9d255901d54ef5df
HP <HP@gmail.com>
2026-05-21 15:25:31 +0530

    kk

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,4 +1,3 @@
-resource "aws_instance" "my_web_server" {
-  ami           = "ami-0c7217cdde317cfec" 
-  instance_type = "t2.micro"              
+module "my_ec2_instance" {
+  source = "./modules/ec2"
 }
